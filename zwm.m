023545e4a3a9c4add0269ecb9831e4649c4e35e3
AlmondLee <754125579@qq.com>
2019-07-04 16:43:00 +0800

    s

--- zwm.m
+++ zwm.m
@@ -1,20 +1,20 @@
 sign=''
 while( sign != "q")
 
-    clear,clc
-    syms s t
-    r=10;
-    c=1300/10^6;
-    hs=1/(1+r*c*s);
-    es=laplace(sqrt(2)*220*exp(-4*t)*cos(10*pi*t));
-    ucs=hs*es;
-    uct=ilaplace(ucs);
-    it=c*diff(uct);
-    t=0.38;
-    subs(uct);
-    eval(ans)
-    subs(it);
-    eval(ans)
+    %clear,clc
+    %syms s t
+    %r=10;
+    %c=1300/10^6;
+    %hs=1/(1+r*c*s);
+    %es=laplace(sqrt(2)*220*exp(-4*t)*cos(10*pi*t));
+    %ucs=hs*es;
+    %uct=ilaplace(ucs);
+    %it=c*diff(uct);
+    %t=0.38;
+    %subs(uct);
+    %eval(ans)
+    %subs(it);
+    %eval(ans)
 
     %fprintf('value of a: %d', a);
     %a = a + 1;
