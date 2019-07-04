f0ef74083672e69f7a8fe25664ec8b92e90cbf33
AlmondLee <754125579@qq.com>
2019-07-04 16:38:39 +0800

    s

---
+++ zwm.m
@@ -0,0 +1,23 @@
+sign=''
+while( sign != "q")
+
+    clear,clc
+    syms s t
+    r=10;
+    c=1300/10^6;
+    hs=1/(1+r*c*s);
+    es=laplace(sqrt(2)*220*exp(-4*t)*cos(10*pi*t));
+    ucs=hs*es;
+    uct=ilaplace(ucs);
+    it=c*diff(uct);
+    t=0.38;
+    subs(uct);
+    eval(ans)
+    subs(it);
+    eval(ans)
+
+    %fprintf('value of a: %d', a);
+    %a = a + 1;
+    input('输入q退出, 回车继续')
+end
+
